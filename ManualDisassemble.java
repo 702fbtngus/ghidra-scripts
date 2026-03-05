28d71ea854e463acf18cfec7655c267452c89573
Suhyeon Ryu <702fbtngus@kaist.ac.kr>
2026-03-05 03:57:19 +0000

    Manual disassemble script added

---
+++ ManualDisassemble.java
@@ -0,0 +1,18 @@
+import ghidra.app.script.GhidraScript;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressSet;
+
+public class ManualDisassemble extends GhidraScript {
+    @Override
+    public void run() throws Exception {
+        // Address start = toAddr(0x401000L);
+        // Address end   = toAddr(0x402000L);
+
+        // AddressSet range = new AddressSet(start, end);
+
+        Address addr = toAddr(0x80005b86L);
+        println("instr (before): " + getInstructionAt(addr));
+        disassemble(addr);
+        println("instr (after): " + getInstructionAt(addr));
+    }
+}
